d41a28debcaf535f5af65088861c502c0876e8e0
Janaina Oliveira <janainaon1@gmail.com>
2020-10-14 22:00:23 -0300

    finalizado upload de imagem com AWS S3

---
+++ src/config/mail.ts
@@ -0,0 +1,23 @@
+import { stringList } from 'aws-sdk/clients/datapipeline';
+
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'noreply@gobarber.com.br',
+      name: 'Equipe GoBarber',
+    },
+  },
+} as IMailConfig;
